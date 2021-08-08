87b5de3598d307d2b1a081fe97b944475aac903e
AzizVirus <aziz07becha@gmail.com>
2021-08-08 09:23:07 +0100

    Create myfirst.js

---
+++ myfirst.js
@@ -0,0 +1,17 @@
+// Require the http module
+var http = require('http');
+
+// Create server function
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+
+  // Text to be written
+  res.write('Hello World!');
+
+  // End server
+  res.end(""); 
+
+  // This line will be ignored because its placed after res.end()
+  res.write('Another line');
+  
+}).listen(8080); // Port
